78de0917fd8e802786a539c4b67aabd0d55872f2
apadron <ariel.padron@travelgenio.com>
2021-09-01 23:39:11 -0300

    Add project files.

---
+++ CarRentalManagement.Client/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using CarRentalManagement.Client.Server.Models;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRentalManagement.Client.Server.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
